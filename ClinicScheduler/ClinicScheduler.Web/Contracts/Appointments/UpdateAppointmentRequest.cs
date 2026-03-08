745c428a0948255b440596540581e466452baf46
Jackson Bailey <jbailey33@leomail.tamuc.edu>
2026-03-07 19:26:51 -0600

    Add request and DTO models for appointments

---
+++ ClinicScheduler/ClinicScheduler.Web/Contracts/Appointments/UpdateAppointmentRequest.cs
@@ -0,0 +1,34 @@
+using System.ComponentModel.DataAnnotations;
+using ClinicScheduler.Core.Entities;
+
+namespace ClinicScheduler.Web.Contracts.Appointments;
+
+public sealed class UpdateAppointmentRequest
+{
+    [Required]
+    [Range(1, int.MaxValue)]
+    public int PatientId { get; init; }
+    
+    [Required]
+    [Range(1, int.MaxValue)]
+    public int TherapistId { get; init; }
+    
+    [Required]
+    [Range(1, int.MaxValue)]
+    public int RoomId { get; init; }
+    
+    [Range(1, int.MaxValue)]
+    public int? TreatmentPlanId { get; init; }
+    
+    [Required]
+    public DateTime StartTime { get; init; }
+    
+    [Required]
+    public DateTime EndTime { get; init; }
+    
+    [Required]
+    public AppointmentStatus Status { get; init; }
+    
+    [StringLength(2000)]
+    public string? Notes { get; init; }
+}
